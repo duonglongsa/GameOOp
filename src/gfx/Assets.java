f560578bc8d0d4e7a3a257561cc312dfe1a2b16a
= <=>
2020-05-06 09:00:22 +0700

    long 5/6

--- src/gfx/Assets.java
+++ src/gfx/Assets.java
@@ -10,31 +10,31 @@ public class Assets {
 	public static BufferedImage[] player_down, player_up, player_left, player_right;
 	
 	public static void innit() {
-		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/Player.png"));
+		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/SwordCharacter.png"));
 		
 		player_down = new BufferedImage[4];
-		player_down[0] = sheet.crop(0, 0, 100, 150);
-		player_down[1] = sheet.crop(100, 0, 100, 150);
-		player_down[2] = sheet.crop(200, 0, 100, 150);
-		player_down[3] = sheet.crop(300, 0, 100, 150);
+		player_down[0] = sheet.crop(20, 500, 50, 40);
+		player_down[1] = sheet.crop(70, 500, 50, 40);
+		player_down[2] = sheet.crop(120, 500, 50, 40);
+		player_down[3] = sheet.crop(170, 500, 50, 40);
 		
 		player_up = new BufferedImage[4];
-		player_up[0] = sheet.crop(0, 150, 100, 150);
-		player_up[1] = sheet.crop(100, 150, 100, 150);
-		player_up[2] = sheet.crop(200, 150, 100, 150);
-		player_up[3] = sheet.crop(300, 150, 100, 150);
+		player_up[0] = sheet.crop(20, 300, 40, 50);
+		player_up[1] = sheet.crop(60, 300, 45, 50);
+		player_up[2] = sheet.crop(105, 300, 45, 50);
+		player_up[3] = sheet.crop(150, 300, 45, 50);
 
 		player_left = new BufferedImage[4];
-		player_left[0] = sheet.crop(0, 300, 100, 150);
-		player_left[1] = sheet.crop(100, 300, 100, 150);
-		player_left[2] = sheet.crop(200, 300, 100, 150);
-		player_left[3] = sheet.crop(300, 300, 100, 150);
+		player_left[0] = sheet.crop(20, 110, 25, 40);
+		player_left[1] = sheet.crop(45, 110, 35, 40);
+		player_left[2] = sheet.crop(80, 110, 30, 40);
+		player_left[3] = sheet.crop(110, 110, 35, 40);
 		
 		player_right = new BufferedImage[4];
-		player_right[0] = sheet.crop(0, 450, 100, 150);
-		player_right[1] = sheet.crop(100, 450, 100, 150);
-		player_right[2] = sheet.crop(200, 450, 100, 150);
-		player_right[3] = sheet.crop(300, 450, 100, 150);
+		player_right[0] = sheet.crop(20, 110, 25, 40);
+		player_right[1] = sheet.crop(45, 110, 35, 40);
+		player_right[2] = sheet.crop(80, 110, 30, 40);
+		player_right[3] = sheet.crop(110, 110, 35, 40);
 		
 		rock = ImageLoader.loadImage("/textures/Rock1.png");
 		lavaRock = ImageLoader.loadImage("/textures/LavaRock.png");
